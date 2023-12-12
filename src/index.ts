c87902686257771679059d18ae3e5e638d9c9ff4
lafkpages <luisafk@proton.me>
2023-12-12 10:43:35 +0100

    Initial commit (v1.0.0)

---
+++ src/index.ts
@@ -0,0 +1,278 @@
+export class File {
+  name: string;
+
+  content?: string | Blob;
+
+  constructor(name: string, content?: string | Blob) {
+    this.name = name;
+    this.content = content;
+  }
+}
+
+export interface Directory {
+  [fileName: string]: Entry;
+}
+
+/**
+ * A file system entry, which can be either a file or a directory.
+ */
+export type Entry = File | Directory;
+
+/**
+ * A file path.
+ *
+ * Can be a string or an array of strings, where each string is a path segment.
+ *
+ * @example
+ * "/path/to/file.txt"
+ *
+ * @example
+ * ["path", "to", "file.txt"]
+ */
+export type Path = string | PathSegments;
+export type PathSegments = readonly string[];
+
+/**
+ * A constant representing the root path.
+ * Can be passed to methods such as {@link MiniFS.readDirectory}.
+ */
+export const rootPath = [] as const;
+
+/**
+ * Converts a {@link Path} to a {@link PathSegments} array.
+ */
+function pathAsSegments(path: Path) {
+  return typeof path == "string" ? path.split("/") : path;
+}
+
+export interface MiniFSOptions {
+  preferErrors?: boolean;
+}
+
+export interface WriteOptions {
+  /**
+   * If true, creates all parent directories if they don't exist.
+   * @default true
+   */
+  recursive?: boolean;
+}
+
+export class MiniFS {
+  protected files: Directory = {};
+
+  // Options
+  protected preferErrors: boolean;
+
+  constructor(options?: MiniFSOptions) {
+    this.preferErrors = options?.preferErrors ?? false;
+  }
+
+  /**
+   * Creates a directory at the given path.
+   * @param recursive If true, creates all parent directories if they don't exist.
+   * @returns A boolean indicating whether the directory was created.
+   */
+  createDirectory(path: Path, options?: WriteOptions) {
+    path = pathAsSegments(path);
+
+    const { recursive } = {
+      recursive: true,
+      ...options,
+    };
+
+    let dir = this.files;
+    for (const pathSegment of path) {
+      if (!(pathSegment in dir)) {
+        if (recursive) {
+          dir[pathSegment] = {};
+        } else if (this.preferErrors) {
+          throw new Error(
+            `[MiniFS.createDirectory] Directory "${pathSegment}" does not exist.`
+          );
+        } else {
+          return false;
+        }
+      }
+
+      const nextEntry = dir[pathSegment];
+
+      if (nextEntry instanceof File) {
+        if (this.preferErrors) {
+          throw new Error(
+            `[MiniFS.createDirectory] "${pathSegment}" is a file.`
+          );
+        }
+        return false;
+      }
+
+      dir = nextEntry;
+    }
+
+    return true;
+  }
+
+  protected readEntry(path: Path) {
+    path = pathAsSegments(path);
+
+    let entry: Entry = this.files;
+    for (const [i, pathSegment] of path.entries()) {
+      if (!(pathSegment in entry)) {
+        return null;
+      }
+
+      const isLastSegment = i == path.length - 1;
+
+      if (entry instanceof File) {
+        if (this.preferErrors) {
+          throw new Error(`[MiniFS.readEntry] "${pathSegment}" is a file.`);
+        }
+        return null;
+      }
+
+      const nextEntry: Entry | undefined = entry[pathSegment];
+
+      if (!isLastSegment && nextEntry instanceof File) {
+        if (this.preferErrors) {
+          throw new Error(`[MiniFS.readEntry] "${pathSegment}" is a file.`);
+        }
+        return null;
+      }
+
+      entry = nextEntry;
+    }
+
+    return entry;
+  }
+
+  readDirectory(path: Path) {
+    const entry = this.readEntry(path);
+
+    if (entry instanceof File) {
+      if (this.preferErrors) {
+        throw new Error(`[MiniFS.readDirectory] "${path}" is a file.`);
+      }
+      return null;
+    }
+
+    if (entry) {
+      return Object.keys(entry);
+    }
+
+    if (this.preferErrors) {
+      throw new Error(`[MiniFS.readDirectory] "${path}" does not exist.`);
+    }
+    return null;
+  }
+
+  readFile(path: Path) {
+    const entry = this.readEntry(path);
+
+    if (entry instanceof File) {
+      if (entry.content == undefined) {
+        if (this.preferErrors) {
+          throw new Error(`[MiniFS.readFile] "${path}" has no content.`);
+        }
+        return null;
+      }
+
+      return entry.content;
+    }
+
+    if (!entry) {
+      if (this.preferErrors) {
+        throw new Error(`[MiniFS.readFile] "${path}" does not exist.`);
+      }
+      return null;
+    }
+
+    if (this.preferErrors) {
+      throw new Error(`[MiniFS.readFile] "${path}" is a directory.`);
+    }
+    return null;
+  }
+
+  /**
+   * Writes a file at the given path.
+   * @param content Optional. The content of the file.
+   * @returns A boolean indicating whether the file was written successfully.
+   */
+  writeFile(path: Path, content?: string | Blob, options?: WriteOptions) {
+    path = pathAsSegments(path);
+
+    const { recursive } = {
+      recursive: true,
+      ...options,
+    };
+
+    let dir = this.files;
+    for (const [i, pathSegment] of path.entries()) {
+      const isLastSegment = i == path.length - 1;
+
+      if (!(pathSegment in dir)) {
+        if (recursive) {
+          if (isLastSegment) {
+            dir[pathSegment] = new File(pathSegment, content);
+            return true;
+          }
+          dir[pathSegment] = {};
+        } else if (this.preferErrors) {
+          throw new Error(
+            `[MiniFS.writeFile] Directory "${pathSegment}" does not exist.`
+          );
+        } else {
+          return false;
+        }
+      }
+
+      const nextEntry = dir[pathSegment];
+
+      if (nextEntry instanceof File) {
+        if (this.preferErrors) {
+          throw new Error(`[MiniFS.writeFile] "${pathSegment}" is a file.`);
+        }
+        return false;
+      }
+
+      dir = nextEntry;
+    }
+
+    return true;
+  }
+
+  remove(path: Path) {
+    path = pathAsSegments(path);
+
+    let dir = this.files;
+    for (const [i, pathSegment] of path.entries()) {
+      const isLastSegment = i == path.length - 1;
+
+      if (!(pathSegment in dir)) {
+        if (this.preferErrors) {
+          throw new Error(
+            `[MiniFS.removeDirectory] Directory "${pathSegment}" does not exist.`
+          );
+        }
+        return false;
+      }
+
+      const nextEntry = dir[pathSegment];
+      if (nextEntry instanceof File) {
+        if (this.preferErrors) {
+          throw new Error(
+            `[MiniFS.removeDirectory] "${pathSegment}" is a file.`
+          );
+        }
+        return false;
+      }
+
+      if (isLastSegment) {
+        delete dir[pathSegment];
+        return true;
+      }
+
+      dir = nextEntry;
+    }
+
+    return true;
+  }
+}
---
+++ src/test/index.test.ts
@@ -0,0 +1,122 @@
+import { expect, test } from "bun:test";
+
+import { MiniFS, rootPath } from "..";
+
+test("createDirectory + readDirectory                     ", () => {
+  const fs = new MiniFS();
+  const path = "myDir";
+
+  // Since the directory does not exist,
+  // it should return null.
+  expect(fs.readDirectory(path)).toBeNull();
+
+  // The directory should now exist.
+  // If if does, it should return true.
+  expect(fs.createDirectory(path)).toBeTrue();
+
+  // Since the directory now exists,
+  // it should return an empty array because
+  // there are no files in the directory.
+  expect(fs.readDirectory(path)).toEqual([]);
+});
+
+test("createDirectory + readDirectory (recursive)         ", () => {
+  const fs = new MiniFS();
+  const path = "myOtherDir/mySubDir";
+
+  expect(fs.createDirectory(path)).toBeTrue();
+
+  // Since the directory now exists,
+  // it should return an empty array because
+  // there are no files in the directory.
+  expect(fs.readDirectory(path)).toEqual([]);
+});
+
+test("createDirectory + readDirectory + remove            ", () => {
+  const fs = new MiniFS();
+  const path = "myDir";
+
+  expect(fs.createDirectory(path)).toBeTrue();
+
+  // Since the directory now exists,
+  // it should return an empty array because
+  // there are no files in the directory.
+  expect(fs.readDirectory(path)).toEqual([]);
+
+  // Since the directory now exists,
+  // removing it should be successful.
+  expect(fs.remove(path)).toBeTrue();
+
+  // Since the directory no longer exists,
+  // it should return null.
+  expect(fs.readDirectory(path)).toBeNull();
+});
+
+test("createDirectory + readDirectory + remove (recursive)", () => {
+  const fs = new MiniFS();
+  const path = "foo/bar";
+
+  expect(fs.createDirectory(path)).toBeTrue();
+
+  // Since the directory now exists,
+  // it should return an empty array because
+  // there are no files in the directory.
+  expect(fs.readDirectory(path)).toEqual([]);
+  expect(fs.readDirectory("foo")).toEqual(["bar"]);
+
+  // Since the directory now exists,
+  // removing it should be successful.
+  expect(fs.remove(path)).toBeTrue();
+
+  // Since the directory no longer exists,
+  // it should return null.
+  expect(fs.readDirectory(path)).toBeNull();
+
+  // Since only the bar subdirectory was removed,
+  // the foo directory should still exist.
+  expect(fs.readDirectory("foo")).toEqual([]);
+});
+
+test("writeFile + readFile + readDirectory                ", () => {
+  const fs = new MiniFS();
+  const path = "myFile.txt";
+  const content = "Hello, World!";
+
+  // Since the file does not exist,
+  // it should return null.
+  expect(fs.readFile(path)).toBeNull();
+
+  // The file should now exist.
+  // If if does, it should return true.
+  expect(fs.writeFile(path, content)).toBeTrue();
+
+  // Since the file now exists,
+  // it should return the file's content.
+  expect(fs.readFile(path)).toBe(content);
+
+  expect(fs.readDirectory(rootPath)).toEqual(["myFile.txt"]);
+});
+
+test("writeFile + readFile + readDirectory (recursive)    ", () => {
+  const fs = new MiniFS();
+  const path = "myParentDir/myFile.txt";
+  const content = "Hello, World!";
+
+  // Since the file does not exist,
+  // it should return null.
+  expect(fs.readFile(path)).toBeNull();
+
+  // The file should now exist.
+  // If if does, it should return true.
+  expect(fs.writeFile(path, content)).toBeTrue();
+
+  // Since the file now exists,
+  // it should return the file's content.
+  expect(fs.readFile(path)).toBe(content);
+
+  // Since recursive is enabled,
+  // the parent directory should exist.
+  expect(fs.readDirectory(rootPath)).toEqual(["myParentDir"]);
+
+  expect(fs.readDirectory(path)).toBeNull();
+});
